ad7b077eb60e64aa8cf4cc53c325fa8f214e40ad
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-24 09:12:43 +0100

    apikey env sample

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
+
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("API_KEY environment variable is not set.")
 
 @app.get("/")
 async def read_index():
