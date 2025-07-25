5bc0badb189e7e8e5fcd60747dad9aefc626ce46
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-07-25 13:04:01 +0100

    modified

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,25 @@
 import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.db import init_db
+from api.chat.routing import router as chat_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app startup
+    init_db()
+    yield
+    # after app startup
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(chat_router, prefix="/api/chats")
 
 API_KEY = os.environ.get("API_KEY")
 if not API_KEY:
     raise NotImplementedError("API_KEY environment variable is not set.")
 
 @app.get("/")
-async def read_index():
+def read_index():
     return {"message": "Welcome to the AI Agent Docker Backend!"}
 
